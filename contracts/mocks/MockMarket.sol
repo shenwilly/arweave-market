ce386e380a391e41ba50b50e5447e75168022969
willy <shen.willy95@gmail.com>
2021-09-03 22:05:18 +0700

    test: update mock market

--- contracts/mocks/MockMarket.sol
+++ contracts/mocks/MockMarket.sol
@@ -5,6 +5,10 @@ import {IMarketMediator} from "../interfaces/IMarketMediator.sol";
 import {IArweaveMarket} from "../interfaces/IArweaveMarket.sol";
 
 contract MockMarket is IArweaveMarket {
+    function createDispute(uint256 _requestId, address _mediator) external {
+        IMarketMediator(_mediator).createDispute(_requestId);
+    }
+
     function resolveDispute(uint256 _requestId, DisputeWinner _winner)
         external
         override
