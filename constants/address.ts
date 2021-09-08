e7243c40e2ca5e4849d2ac17f83f71c6a03a9792
willy <shen.willy95@gmail.com>
2021-09-08 09:37:01 +0700

    feat: add arbitrator contract addresses

--- constants/address.ts
+++ constants/address.ts
@@ -5,3 +5,8 @@ export const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 export const USDC_MINTER = "0xae2d4617c862309a3d75a0ffb358c7a5009c673f";
 
 export const DUMMY_ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+
+export const ARBITRATOR_ADDRESS: { [key: number]: string } = {
+  1: "0x988b3a538b618c7a603e1c11ab82cd16dbe28069",
+  100: "0x9C1dA9A04925bDfDedf0f6421bC7EEa8305F9002",
+};
